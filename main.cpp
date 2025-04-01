244c1d7a749e7441b54ef31554591574fe7ff7a5
ValentinoAbinata <valentabinata@gmail.com>
2025-04-01 22:05:24 +0700

    folder vscode

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int a = 47;
+int b = 33;
+int c = 12;
+int d = 50;
+int e = 9;
+int f = 49;
+int g = 39;
+int h = 30;
+
+int main(){
+
+    cout << a % 9 << endl ;
+    cout << b % 9 << endl ;
+    cout << c % 9 << endl ;
+    cout << d % 9 << endl ;
+    cout << e % 9 << endl ;
+    cout << f % 9 << endl ;
+    cout << g % 9 << endl ;
+    cout << h % 9 << endl ;
+}
